a8903234817c07c970c27fca9d9015cb38a940ac
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:22:07 -0500

    Add files via upload

---
+++ 05_Load Event/Load Event/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Load_Event
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.countriesListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(22, 20);
+            this.outputDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(321, 25);
+            this.outputDescriptionLabel.TabIndex = 5;
+            this.outputDescriptionLabel.Text = "The Countries of South America";
+            // 
+            // countriesListBox
+            // 
+            this.countriesListBox.FormattingEnabled = true;
+            this.countriesListBox.ItemHeight = 20;
+            this.countriesListBox.Location = new System.Drawing.Point(64, 62);
+            this.countriesListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.countriesListBox.Name = "countriesListBox";
+            this.countriesListBox.Size = new System.Drawing.Size(254, 144);
+            this.countriesListBox.TabIndex = 4;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(135, 232);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 35);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 291);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.countriesListBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "South America";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.ListBox countriesListBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
