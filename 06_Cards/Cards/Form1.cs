db39b5151880d19875388501af449c112d4a7b18
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:25:33 -0500

    Add files via upload

---
+++ 06_Cards/Cards/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The ShowCard method accepts a string tha tnames
+        // the selecte card, and displays that card.
+        private void ShowCard(string card)
+        {
+            switch (card)
+            {
+                case "Ace of Spades":
+                    ShowAceSpades();
+                    break;
+                case "10 of Hearts":
+                    ShowTenHearts();
+                    break;
+                case "King of Clubs":
+                    ShowKingClubs();
+                    break;
+            }
+        }
+        // The ShowAceSpades Method makes the Ace of Spade
+        // visible and the other cards invisible.
+        private void ShowAceSpades()
+        {
+            aceSpadesPictureBox.Visible = true;
+            tenHeartsPictureBox.Visible = false;
+            kingClubsPictureBox.Visible = false;
+        }
+        // The ShowTenHearts method makes the Ten of Hearts
+        // visible and the other cards invisibile.
+        private void ShowTenHearts()
+        {
+            tenHeartsPictureBox.Visible = true;
+            aceSpadesPictureBox.Visible = false;
+            kingClubsPictureBox.Visible = false;
+        }
+        // The ShowKingCLubs method makes the King of Clubs
+        // visible and the other cards invisible
+        private void ShowKingClubs()
+        {
+            kingClubsPictureBox.Visible = true;
+            aceSpadesPictureBox.Visible = false;
+            tenHeartsPictureBox.Visible = false;
+        }
+
+        private void showCardButton_Click(object sender, EventArgs e)
+        {
+            // If a card is slected int he ListBox, display it.
+            if (cardListBox.SelectedIndex != -1)
+            {
+                ShowCard(cardListBox.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please select a card from " +
+                    "the list box.");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
