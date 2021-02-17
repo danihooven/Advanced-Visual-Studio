0c132e7aaa6310bf1eef9ff3671ce9211478c602
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:29:11 -0500

    Add files via upload

---
+++ 06_Lights/Lights/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lights
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TurnLightOn()
+        {
+            // Display the "light on" image.
+            lightOnPictureBox.Visible = true;
+
+            // Hide the "light off" image.
+            lightOffPictureBox.Visible = false;
+
+            // Display the light's on state.
+            lightStateLabel.Text = "ON";
+        }
+
+        private void TurnLightOff()
+        {
+            // Display the "light off" image.
+            lightOffPictureBox.Visible = true;
+
+            // Hide the "light on" image.
+            lightOnPictureBox.Visible = false;
+
+            // Display the light's state
+            lightStateLabel.Text = "OFF";
+        }
+
+        private void switchButton_Click(object sender, EventArgs e)
+        {
+            // Reverse the state of hte light.
+            if (lightOnPictureBox.Visible == true)
+            {
+                TurnLightOff();
+            }
+            else
+            {
+                TurnLightOn();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
