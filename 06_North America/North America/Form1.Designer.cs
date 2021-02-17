4796001e77c986ffd361de5eceaece90d47fc2c9
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:34:49 -0500

    Add files via upload

---
+++ 06_North America/North America/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace North_America
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.getCountriesButton = new System.Windows.Forms.Button();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.countriesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(204, 237);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 60);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // getCountriesButton
+            // 
+            this.getCountriesButton.Location = new System.Drawing.Point(82, 237);
+            this.getCountriesButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.getCountriesButton.Name = "getCountriesButton";
+            this.getCountriesButton.Size = new System.Drawing.Size(112, 60);
+            this.getCountriesButton.TabIndex = 6;
+            this.getCountriesButton.Text = "Get Countries";
+            this.getCountriesButton.UseVisualStyleBackColor = true;
+            this.getCountriesButton.Click += new System.EventHandler(this.getCountriesButton_Click);
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(22, 20);
+            this.outputDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(316, 25);
+            this.outputDescriptionLabel.TabIndex = 5;
+            this.outputDescriptionLabel.Text = "The Countries of North America";
+            // 
+            // countriesListBox
+            // 
+            this.countriesListBox.FormattingEnabled = true;
+            this.countriesListBox.ItemHeight = 20;
+            this.countriesListBox.Location = new System.Drawing.Point(64, 62);
+            this.countriesListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.countriesListBox.Name = "countriesListBox";
+            this.countriesListBox.Size = new System.Drawing.Size(254, 144);
+            this.countriesListBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 317);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.getCountriesButton);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.countriesListBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "North America";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button getCountriesButton;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.ListBox countriesListBox;
+    }
+}
+
---
+++ 06_North America/North America/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace North_America
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The GetFileName method gets a filename from the
+        // user and assigns it to the variable passed as
+        // and arguemnt.
+        private void GetFileName(out string selectedFile)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                selectedFile = openFile.FileName;
+            }
+            else
+            {
+                selectedFile = "";
+            }
+        }
+
+        // The GetCountries method accepts a filename as an
+        // argument. It opensthe specified file and ispalys
+        // its contents in the countiesLIstBox control.
+        private void GetCountries(string filename)
+        {
+            try
+            {
+                // Declare a variable to hold a country name.
+                string countryName;
+
+                // Decalre a StringReader variable.
+                StreamReader inputFile;
+
+                // Open the file and get a StreamReader object.
+                inputFile = File.OpenText(filename);
+
+                // Clear anything currently in the ListBox.
+                countriesListBox.Items.Clear();
+
+                // Read the file's contents.
+                while (!inputFile.EndOfStream)
+                {
+                    // Get a country name.
+                    countryName = inputFile.ReadLine();
+
+                    // Add the country anme to the ListBox.
+                    countriesListBox.Items.Add(countryName);
+
+                    // Close the file.
+                    inputFile.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Display an error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void getCountriesButton_Click(object sender, EventArgs e)
+        {
+            string filename;        // To hold the filename
+
+            // Get the filename from the user.
+            GetFileName(out filename);
+
+            // Get the countries from the file.
+            GetCountries(filename);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
