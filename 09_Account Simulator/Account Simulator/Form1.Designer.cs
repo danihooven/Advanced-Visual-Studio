2753f6e31c7382af9e954ed23101466ba5a27f27
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:42:16 -0500

    Add files via upload

---
+++ 09_Account Simulator/Account Simulator/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Account_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.depositGroupBox = new System.Windows.Forms.GroupBox();
+            this.depositButton = new System.Windows.Forms.Button();
+            this.depositTextBox = new System.Windows.Forms.TextBox();
+            this.depositAmountDescriptionLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.withdrawButton = new System.Windows.Forms.Button();
+            this.withdrawTextBox = new System.Windows.Forms.TextBox();
+            this.withdrawAmountDescriptionLabel = new System.Windows.Forms.Label();
+            this.balanceDescriptionLabel = new System.Windows.Forms.Label();
+            this.balanceLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.depositGroupBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // depositGroupBox
+            // 
+            this.depositGroupBox.Controls.Add(this.depositButton);
+            this.depositGroupBox.Controls.Add(this.depositTextBox);
+            this.depositGroupBox.Controls.Add(this.depositAmountDescriptionLabel);
+            this.depositGroupBox.Location = new System.Drawing.Point(18, 102);
+            this.depositGroupBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.depositGroupBox.Name = "depositGroupBox";
+            this.depositGroupBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.depositGroupBox.Size = new System.Drawing.Size(226, 122);
+            this.depositGroupBox.TabIndex = 0;
+            this.depositGroupBox.TabStop = false;
+            this.depositGroupBox.Text = "Make a Deposit";
+            // 
+            // depositButton
+            // 
+            this.depositButton.Location = new System.Drawing.Point(57, 71);
+            this.depositButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.depositButton.Name = "depositButton";
+            this.depositButton.Size = new System.Drawing.Size(112, 35);
+            this.depositButton.TabIndex = 2;
+            this.depositButton.Text = "Deposit";
+            this.depositButton.UseVisualStyleBackColor = true;
+            this.depositButton.Click += new System.EventHandler(this.depositButton_Click);
+            // 
+            // depositTextBox
+            // 
+            this.depositTextBox.Location = new System.Drawing.Point(102, 31);
+            this.depositTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.depositTextBox.Name = "depositTextBox";
+            this.depositTextBox.Size = new System.Drawing.Size(98, 26);
+            this.depositTextBox.TabIndex = 1;
+            // 
+            // depositAmountDescriptionLabel
+            // 
+            this.depositAmountDescriptionLabel.AutoSize = true;
+            this.depositAmountDescriptionLabel.Location = new System.Drawing.Point(24, 35);
+            this.depositAmountDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.depositAmountDescriptionLabel.Name = "depositAmountDescriptionLabel";
+            this.depositAmountDescriptionLabel.Size = new System.Drawing.Size(69, 20);
+            this.depositAmountDescriptionLabel.TabIndex = 0;
+            this.depositAmountDescriptionLabel.Text = "Amount:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.withdrawButton);
+            this.groupBox1.Controls.Add(this.withdrawTextBox);
+            this.groupBox1.Controls.Add(this.withdrawAmountDescriptionLabel);
+            this.groupBox1.Location = new System.Drawing.Point(254, 102);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(226, 122);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Make a Withdrawal";
+            // 
+            // withdrawButton
+            // 
+            this.withdrawButton.Location = new System.Drawing.Point(57, 71);
+            this.withdrawButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.withdrawButton.Name = "withdrawButton";
+            this.withdrawButton.Size = new System.Drawing.Size(112, 35);
+            this.withdrawButton.TabIndex = 2;
+            this.withdrawButton.Text = "Withdraw";
+            this.withdrawButton.UseVisualStyleBackColor = true;
+            this.withdrawButton.Click += new System.EventHandler(this.withdrawButton_Click);
+            // 
+            // withdrawTextBox
+            // 
+            this.withdrawTextBox.Location = new System.Drawing.Point(102, 31);
+            this.withdrawTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.withdrawTextBox.Name = "withdrawTextBox";
+            this.withdrawTextBox.Size = new System.Drawing.Size(98, 26);
+            this.withdrawTextBox.TabIndex = 1;
+            // 
+            // withdrawAmountDescriptionLabel
+            // 
+            this.withdrawAmountDescriptionLabel.AutoSize = true;
+            this.withdrawAmountDescriptionLabel.Location = new System.Drawing.Point(24, 35);
+            this.withdrawAmountDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.withdrawAmountDescriptionLabel.Name = "withdrawAmountDescriptionLabel";
+            this.withdrawAmountDescriptionLabel.Size = new System.Drawing.Size(69, 20);
+            this.withdrawAmountDescriptionLabel.TabIndex = 0;
+            this.withdrawAmountDescriptionLabel.Text = "Amount:";
+            // 
+            // balanceDescriptionLabel
+            // 
+            this.balanceDescriptionLabel.AutoSize = true;
+            this.balanceDescriptionLabel.Location = new System.Drawing.Point(134, 45);
+            this.balanceDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.balanceDescriptionLabel.Name = "balanceDescriptionLabel";
+            this.balanceDescriptionLabel.Size = new System.Drawing.Size(71, 20);
+            this.balanceDescriptionLabel.TabIndex = 2;
+            this.balanceDescriptionLabel.Text = "Balance:";
+            // 
+            // balanceLabel
+            // 
+            this.balanceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.balanceLabel.Location = new System.Drawing.Point(216, 37);
+            this.balanceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.balanceLabel.Name = "balanceLabel";
+            this.balanceLabel.Size = new System.Drawing.Size(149, 34);
+            this.balanceLabel.TabIndex = 3;
+            this.balanceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(194, 249);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 35);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 311);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.balanceLabel);
+            this.Controls.Add(this.balanceDescriptionLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.depositGroupBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Account Simulator";
+            this.depositGroupBox.ResumeLayout(false);
+            this.depositGroupBox.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox depositGroupBox;
+        private System.Windows.Forms.Button depositButton;
+        private System.Windows.Forms.TextBox depositTextBox;
+        private System.Windows.Forms.Label depositAmountDescriptionLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button withdrawButton;
+        private System.Windows.Forms.TextBox withdrawTextBox;
+        private System.Windows.Forms.Label withdrawAmountDescriptionLabel;
+        private System.Windows.Forms.Label balanceDescriptionLabel;
+        private System.Windows.Forms.Label balanceLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ 09_Account Simulator/Account Simulator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Account_Simulator
+{
+    public partial class Form1 : Form
+    {
+
+        // BankAccount field with a $1000 starting balance
+        private BankAccount account = new BankAccount(1000);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            decimal amount; // To hold the amount of deposit
+
+            // Convert the amount to a decimal.
+            if (decimal.TryParse(depositTextBox.Text, out amount))
+            {
+                // Deposit the amount into the account.
+                account.Deposit(amount);
+
+                // Display the new balance.
+                balanceLabel.Text = account.Balance.ToString("c");
+
+                // Clear the text box.
+                depositTextBox.Clear();
+            }
+            else
+            {
+                // Display an error message.
+                MessageBox.Show("Invalid amount");
+            }
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            decimal amount;     // To hold the amount of withdrawl
+
+            // Convert the amount to a decimal.
+            if (decimal.TryParse(withdrawTextBox.Text, out amount))
+            {
+                // Withdraw the amount from the account.
+                account.Withdraw(amount);
+
+                // Display the new balance.
+                balanceLabel.Text = account.Balance.ToString("c");
+
+                // Clear the text box.
+                withdrawTextBox.Clear();
+            }
+            else
+            {
+                // Display an error message.
+                MessageBox.Show("invalid amount");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
