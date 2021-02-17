00b6d156c902eb155474df213297af9b1ddfc2a3
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:41:37 -0500

    Add files via upload

---
+++ 08_Word Counter/Word Counter/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Word_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stringTextBox = new System.Windows.Forms.TextBox();
+            this.countButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // stringTextBox
+            // 
+            this.stringTextBox.AllowDrop = true;
+            this.stringTextBox.Location = new System.Drawing.Point(50, 35);
+            this.stringTextBox.Multiline = true;
+            this.stringTextBox.Name = "stringTextBox";
+            this.stringTextBox.Size = new System.Drawing.Size(334, 127);
+            this.stringTextBox.TabIndex = 0;
+            // 
+            // countButton
+            // 
+            this.countButton.Location = new System.Drawing.Point(90, 198);
+            this.countButton.Name = "countButton";
+            this.countButton.Size = new System.Drawing.Size(120, 68);
+            this.countButton.TabIndex = 1;
+            this.countButton.Text = "Word Count";
+            this.countButton.UseVisualStyleBackColor = true;
+            this.countButton.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(232, 198);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(120, 68);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 300);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.countButton);
+            this.Controls.Add(this.stringTextBox);
+            this.Name = "Form1";
+            this.Text = "Word Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox stringTextBox;
+        private System.Windows.Forms.Button countButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ 08_Word Counter/Word Counter/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Word_Counter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The WordCount method accepts a string agrument
+        // and returns the number of words it contains.
+        private int WordCounter(string str)
+        {
+            int wordCount = 0;      // Word counter
+
+            // Trim the string.
+            str = str.Trim();
+
+            // Count the number of words in str.
+            foreach (char ch in str)
+            {
+                if (char.IsWhiteSpace(ch))
+                {
+                    wordCount++;
+                }
+            }
+
+            // Return the total word count.
+            return wordCount + 1;
+                        
+        }
+
+        private void countButton_Click(object sender, EventArgs e)
+        {
+            // Get the string from the TextBox
+            string str = stringTextBox.Text;
+
+            // Validate the string
+            if (str.Length >= 1)
+            {
+                // Get the total word count
+                int totalWordCount = WordCounter(str);
+
+                // Display the total word count
+                MessageBox.Show("The total number of words is " + totalWordCount.ToString());
+            }
+        }
+    }
+}
