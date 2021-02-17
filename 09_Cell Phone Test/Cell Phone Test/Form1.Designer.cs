890d29096a95b61dbfe3b676e493ff2204ac5ebf
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:43:43 -0500

    Add files via upload

---
+++ 09_Cell Phone Test/Cell Phone Test/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Cell_Phone_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterDataGroupBox = new System.Windows.Forms.GroupBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.modelTextBox = new System.Windows.Forms.TextBox();
+            this.brandTextBox = new System.Windows.Forms.TextBox();
+            this.promptPriceLabel = new System.Windows.Forms.Label();
+            this.promptModelLabel = new System.Windows.Forms.Label();
+            this.promptBrandLabel = new System.Windows.Forms.Label();
+            this.objectPropertiesGroupBox = new System.Windows.Forms.GroupBox();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.modelLabel = new System.Windows.Forms.Label();
+            this.brandLabel = new System.Windows.Forms.Label();
+            this.outputPriceLabel = new System.Windows.Forms.Label();
+            this.outputModelLabel = new System.Windows.Forms.Label();
+            this.outputBrandLabel = new System.Windows.Forms.Label();
+            this.createObjectButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.enterDataGroupBox.SuspendLayout();
+            this.objectPropertiesGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // enterDataGroupBox
+            // 
+            this.enterDataGroupBox.Controls.Add(this.priceTextBox);
+            this.enterDataGroupBox.Controls.Add(this.modelTextBox);
+            this.enterDataGroupBox.Controls.Add(this.brandTextBox);
+            this.enterDataGroupBox.Controls.Add(this.promptPriceLabel);
+            this.enterDataGroupBox.Controls.Add(this.promptModelLabel);
+            this.enterDataGroupBox.Controls.Add(this.promptBrandLabel);
+            this.enterDataGroupBox.Location = new System.Drawing.Point(51, 18);
+            this.enterDataGroupBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.enterDataGroupBox.Name = "enterDataGroupBox";
+            this.enterDataGroupBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.enterDataGroupBox.Size = new System.Drawing.Size(279, 154);
+            this.enterDataGroupBox.TabIndex = 0;
+            this.enterDataGroupBox.TabStop = false;
+            this.enterDataGroupBox.Text = "Enter Cell Phone Data";
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(93, 114);
+            this.priceTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(148, 26);
+            this.priceTextBox.TabIndex = 5;
+            // 
+            // modelTextBox
+            // 
+            this.modelTextBox.Location = new System.Drawing.Point(93, 74);
+            this.modelTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.modelTextBox.Name = "modelTextBox";
+            this.modelTextBox.Size = new System.Drawing.Size(148, 26);
+            this.modelTextBox.TabIndex = 4;
+            // 
+            // brandTextBox
+            // 
+            this.brandTextBox.Location = new System.Drawing.Point(93, 34);
+            this.brandTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.brandTextBox.Name = "brandTextBox";
+            this.brandTextBox.Size = new System.Drawing.Size(148, 26);
+            this.brandTextBox.TabIndex = 3;
+            // 
+            // promptPriceLabel
+            // 
+            this.promptPriceLabel.AutoSize = true;
+            this.promptPriceLabel.Location = new System.Drawing.Point(33, 118);
+            this.promptPriceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.promptPriceLabel.Name = "promptPriceLabel";
+            this.promptPriceLabel.Size = new System.Drawing.Size(48, 20);
+            this.promptPriceLabel.TabIndex = 2;
+            this.promptPriceLabel.Text = "Price:";
+            // 
+            // promptModelLabel
+            // 
+            this.promptModelLabel.AutoSize = true;
+            this.promptModelLabel.Location = new System.Drawing.Point(26, 80);
+            this.promptModelLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.promptModelLabel.Name = "promptModelLabel";
+            this.promptModelLabel.Size = new System.Drawing.Size(56, 20);
+            this.promptModelLabel.TabIndex = 1;
+            this.promptModelLabel.Text = "Model:";
+            // 
+            // promptBrandLabel
+            // 
+            this.promptBrandLabel.AutoSize = true;
+            this.promptBrandLabel.Location = new System.Drawing.Point(27, 38);
+            this.promptBrandLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.promptBrandLabel.Name = "promptBrandLabel";
+            this.promptBrandLabel.Size = new System.Drawing.Size(56, 20);
+            this.promptBrandLabel.TabIndex = 0;
+            this.promptBrandLabel.Text = "Brand:";
+            // 
+            // objectPropertiesGroupBox
+            // 
+            this.objectPropertiesGroupBox.Controls.Add(this.priceLabel);
+            this.objectPropertiesGroupBox.Controls.Add(this.modelLabel);
+            this.objectPropertiesGroupBox.Controls.Add(this.brandLabel);
+            this.objectPropertiesGroupBox.Controls.Add(this.outputPriceLabel);
+            this.objectPropertiesGroupBox.Controls.Add(this.outputModelLabel);
+            this.objectPropertiesGroupBox.Controls.Add(this.outputBrandLabel);
+            this.objectPropertiesGroupBox.Location = new System.Drawing.Point(51, 182);
+            this.objectPropertiesGroupBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.objectPropertiesGroupBox.Name = "objectPropertiesGroupBox";
+            this.objectPropertiesGroupBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.objectPropertiesGroupBox.Size = new System.Drawing.Size(279, 191);
+            this.objectPropertiesGroupBox.TabIndex = 1;
+            this.objectPropertiesGroupBox.TabStop = false;
+            this.objectPropertiesGroupBox.Text = "Object Properties";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.priceLabel.Location = new System.Drawing.Point(88, 131);
+            this.priceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(149, 34);
+            this.priceLabel.TabIndex = 7;
+            this.priceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // modelLabel
+            // 
+            this.modelLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.modelLabel.Location = new System.Drawing.Point(88, 85);
+            this.modelLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.modelLabel.Name = "modelLabel";
+            this.modelLabel.Size = new System.Drawing.Size(149, 34);
+            this.modelLabel.TabIndex = 6;
+            this.modelLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // brandLabel
+            // 
+            this.brandLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.brandLabel.Location = new System.Drawing.Point(88, 38);
+            this.brandLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.brandLabel.Name = "brandLabel";
+            this.brandLabel.Size = new System.Drawing.Size(149, 34);
+            this.brandLabel.TabIndex = 4;
+            this.brandLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputPriceLabel
+            // 
+            this.outputPriceLabel.AutoSize = true;
+            this.outputPriceLabel.Location = new System.Drawing.Point(33, 138);
+            this.outputPriceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputPriceLabel.Name = "outputPriceLabel";
+            this.outputPriceLabel.Size = new System.Drawing.Size(48, 20);
+            this.outputPriceLabel.TabIndex = 5;
+            this.outputPriceLabel.Text = "Price:";
+            // 
+            // outputModelLabel
+            // 
+            this.outputModelLabel.AutoSize = true;
+            this.outputModelLabel.Location = new System.Drawing.Point(26, 92);
+            this.outputModelLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputModelLabel.Name = "outputModelLabel";
+            this.outputModelLabel.Size = new System.Drawing.Size(56, 20);
+            this.outputModelLabel.TabIndex = 4;
+            this.outputModelLabel.Text = "Model:";
+            // 
+            // outputBrandLabel
+            // 
+            this.outputBrandLabel.AutoSize = true;
+            this.outputBrandLabel.Location = new System.Drawing.Point(27, 45);
+            this.outputBrandLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputBrandLabel.Name = "outputBrandLabel";
+            this.outputBrandLabel.Size = new System.Drawing.Size(56, 20);
+            this.outputBrandLabel.TabIndex = 3;
+            this.outputBrandLabel.Text = "Brand:";
+            // 
+            // createObjectButton
+            // 
+            this.createObjectButton.Location = new System.Drawing.Point(74, 391);
+            this.createObjectButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.createObjectButton.Name = "createObjectButton";
+            this.createObjectButton.Size = new System.Drawing.Size(112, 63);
+            this.createObjectButton.TabIndex = 2;
+            this.createObjectButton.Text = "Create Object";
+            this.createObjectButton.UseVisualStyleBackColor = true;
+            this.createObjectButton.Click += new System.EventHandler(this.createObjectButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(195, 391);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 63);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 472);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.createObjectButton);
+            this.Controls.Add(this.objectPropertiesGroupBox);
+            this.Controls.Add(this.enterDataGroupBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Cell Phone Test";
+            this.enterDataGroupBox.ResumeLayout(false);
+            this.enterDataGroupBox.PerformLayout();
+            this.objectPropertiesGroupBox.ResumeLayout(false);
+            this.objectPropertiesGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox enterDataGroupBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox modelTextBox;
+        private System.Windows.Forms.TextBox brandTextBox;
+        private System.Windows.Forms.Label promptPriceLabel;
+        private System.Windows.Forms.Label promptModelLabel;
+        private System.Windows.Forms.Label promptBrandLabel;
+        private System.Windows.Forms.GroupBox objectPropertiesGroupBox;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label modelLabel;
+        private System.Windows.Forms.Label brandLabel;
+        private System.Windows.Forms.Label outputPriceLabel;
+        private System.Windows.Forms.Label outputModelLabel;
+        private System.Windows.Forms.Label outputBrandLabel;
+        private System.Windows.Forms.Button createObjectButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
