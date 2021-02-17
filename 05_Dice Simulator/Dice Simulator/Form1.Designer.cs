1568d25eca2d6d1f497890ec2e6c4d1bfbad3c28
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:19:51 -0500

    Add files via upload

---
+++ 05_Dice Simulator/Dice Simulator/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Dice_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.diePictureBox1_1 = new System.Windows.Forms.PictureBox();
+            this.rollButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.diePictureBox2_1 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox3_1 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox4_1 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox5_1 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox6_1 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox1_2 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox2_2 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox3_2 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox4_2 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox5_2 = new System.Windows.Forms.PictureBox();
+            this.diePictureBox6_2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox1_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox2_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox3_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox4_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox5_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox6_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox1_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox2_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox3_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox4_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox5_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox6_2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // diePictureBox1_1
+            // 
+            this.diePictureBox1_1.Image = global::Dice_Simulator.Properties.Resources.Die1;
+            this.diePictureBox1_1.Location = new System.Drawing.Point(39, 12);
+            this.diePictureBox1_1.Name = "diePictureBox1_1";
+            this.diePictureBox1_1.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox1_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox1_1.TabIndex = 0;
+            this.diePictureBox1_1.TabStop = false;
+            this.diePictureBox1_1.Visible = false;
+            // 
+            // rollButton
+            // 
+            this.rollButton.Location = new System.Drawing.Point(23, 143);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(130, 47);
+            this.rollButton.TabIndex = 6;
+            this.rollButton.Text = "Roll";
+            this.rollButton.UseVisualStyleBackColor = true;
+            this.rollButton.Click += new System.EventHandler(this.rollButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(202, 143);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(130, 47);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // diePictureBox2_1
+            // 
+            this.diePictureBox2_1.Image = global::Dice_Simulator.Properties.Resources.Die2;
+            this.diePictureBox2_1.Location = new System.Drawing.Point(39, 12);
+            this.diePictureBox2_1.Name = "diePictureBox2_1";
+            this.diePictureBox2_1.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox2_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox2_1.TabIndex = 8;
+            this.diePictureBox2_1.TabStop = false;
+            this.diePictureBox2_1.Visible = false;
+            // 
+            // diePictureBox3_1
+            // 
+            this.diePictureBox3_1.Image = global::Dice_Simulator.Properties.Resources.Die3;
+            this.diePictureBox3_1.Location = new System.Drawing.Point(39, 12);
+            this.diePictureBox3_1.Name = "diePictureBox3_1";
+            this.diePictureBox3_1.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox3_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox3_1.TabIndex = 9;
+            this.diePictureBox3_1.TabStop = false;
+            this.diePictureBox3_1.Visible = false;
+            // 
+            // diePictureBox4_1
+            // 
+            this.diePictureBox4_1.Image = global::Dice_Simulator.Properties.Resources.Die4;
+            this.diePictureBox4_1.Location = new System.Drawing.Point(39, 12);
+            this.diePictureBox4_1.Name = "diePictureBox4_1";
+            this.diePictureBox4_1.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox4_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox4_1.TabIndex = 10;
+            this.diePictureBox4_1.TabStop = false;
+            this.diePictureBox4_1.Visible = false;
+            // 
+            // diePictureBox5_1
+            // 
+            this.diePictureBox5_1.Image = global::Dice_Simulator.Properties.Resources.Die5;
+            this.diePictureBox5_1.Location = new System.Drawing.Point(39, 12);
+            this.diePictureBox5_1.Name = "diePictureBox5_1";
+            this.diePictureBox5_1.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox5_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox5_1.TabIndex = 11;
+            this.diePictureBox5_1.TabStop = false;
+            this.diePictureBox5_1.Visible = false;
+            // 
+            // diePictureBox6_1
+            // 
+            this.diePictureBox6_1.Image = global::Dice_Simulator.Properties.Resources.Die6;
+            this.diePictureBox6_1.Location = new System.Drawing.Point(39, 12);
+            this.diePictureBox6_1.Name = "diePictureBox6_1";
+            this.diePictureBox6_1.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox6_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox6_1.TabIndex = 12;
+            this.diePictureBox6_1.TabStop = false;
+            this.diePictureBox6_1.Visible = false;
+            // 
+            // diePictureBox1_2
+            // 
+            this.diePictureBox1_2.Image = global::Dice_Simulator.Properties.Resources.Die6;
+            this.diePictureBox1_2.Location = new System.Drawing.Point(219, 12);
+            this.diePictureBox1_2.Name = "diePictureBox1_2";
+            this.diePictureBox1_2.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox1_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox1_2.TabIndex = 18;
+            this.diePictureBox1_2.TabStop = false;
+            this.diePictureBox1_2.Visible = false;
+            // 
+            // diePictureBox2_2
+            // 
+            this.diePictureBox2_2.Image = global::Dice_Simulator.Properties.Resources.Die5;
+            this.diePictureBox2_2.Location = new System.Drawing.Point(219, 12);
+            this.diePictureBox2_2.Name = "diePictureBox2_2";
+            this.diePictureBox2_2.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox2_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox2_2.TabIndex = 17;
+            this.diePictureBox2_2.TabStop = false;
+            this.diePictureBox2_2.Visible = false;
+            // 
+            // diePictureBox3_2
+            // 
+            this.diePictureBox3_2.Image = global::Dice_Simulator.Properties.Resources.Die4;
+            this.diePictureBox3_2.Location = new System.Drawing.Point(219, 12);
+            this.diePictureBox3_2.Name = "diePictureBox3_2";
+            this.diePictureBox3_2.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox3_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox3_2.TabIndex = 16;
+            this.diePictureBox3_2.TabStop = false;
+            this.diePictureBox3_2.Visible = false;
+            // 
+            // diePictureBox4_2
+            // 
+            this.diePictureBox4_2.Image = global::Dice_Simulator.Properties.Resources.Die3;
+            this.diePictureBox4_2.Location = new System.Drawing.Point(219, 12);
+            this.diePictureBox4_2.Name = "diePictureBox4_2";
+            this.diePictureBox4_2.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox4_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox4_2.TabIndex = 15;
+            this.diePictureBox4_2.TabStop = false;
+            this.diePictureBox4_2.Visible = false;
+            // 
+            // diePictureBox5_2
+            // 
+            this.diePictureBox5_2.Image = global::Dice_Simulator.Properties.Resources.Die2;
+            this.diePictureBox5_2.Location = new System.Drawing.Point(219, 12);
+            this.diePictureBox5_2.Name = "diePictureBox5_2";
+            this.diePictureBox5_2.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox5_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox5_2.TabIndex = 14;
+            this.diePictureBox5_2.TabStop = false;
+            this.diePictureBox5_2.Visible = false;
+            // 
+            // diePictureBox6_2
+            // 
+            this.diePictureBox6_2.Image = global::Dice_Simulator.Properties.Resources.Die1;
+            this.diePictureBox6_2.Location = new System.Drawing.Point(219, 12);
+            this.diePictureBox6_2.Name = "diePictureBox6_2";
+            this.diePictureBox6_2.Size = new System.Drawing.Size(100, 100);
+            this.diePictureBox6_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePictureBox6_2.TabIndex = 13;
+            this.diePictureBox6_2.TabStop = false;
+            this.diePictureBox6_2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 215);
+            this.Controls.Add(this.diePictureBox1_2);
+            this.Controls.Add(this.diePictureBox2_2);
+            this.Controls.Add(this.diePictureBox3_2);
+            this.Controls.Add(this.diePictureBox4_2);
+            this.Controls.Add(this.diePictureBox5_2);
+            this.Controls.Add(this.diePictureBox6_2);
+            this.Controls.Add(this.diePictureBox6_1);
+            this.Controls.Add(this.diePictureBox5_1);
+            this.Controls.Add(this.diePictureBox4_1);
+            this.Controls.Add(this.diePictureBox3_1);
+            this.Controls.Add(this.diePictureBox2_1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.rollButton);
+            this.Controls.Add(this.diePictureBox1_1);
+            this.Name = "Form1";
+            this.Text = "Dice Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox1_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox2_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox3_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox4_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox5_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox6_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox1_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox2_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox3_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox4_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox5_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePictureBox6_2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox diePictureBox1_1;
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox diePictureBox2_1;
+        private System.Windows.Forms.PictureBox diePictureBox3_1;
+        private System.Windows.Forms.PictureBox diePictureBox4_1;
+        private System.Windows.Forms.PictureBox diePictureBox5_1;
+        private System.Windows.Forms.PictureBox diePictureBox6_1;
+        private System.Windows.Forms.PictureBox diePictureBox1_2;
+        private System.Windows.Forms.PictureBox diePictureBox2_2;
+        private System.Windows.Forms.PictureBox diePictureBox3_2;
+        private System.Windows.Forms.PictureBox diePictureBox4_2;
+        private System.Windows.Forms.PictureBox diePictureBox5_2;
+        private System.Windows.Forms.PictureBox diePictureBox6_2;
+    }
+}
+
---
+++ 05_Dice Simulator/Dice Simulator/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dice_Simulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rollButton_Click(object sender, EventArgs e)
+        {
+            // Variable to indicate dice value
+            int dieValue_1;
+            int dieValue_2;
+
+            // Create a Random object.
+            Random rand_1 = new Random();
+            Random rand_2 = new Random();
+
+            // Get a random integer in the range of 0 through 5.
+            // 0 = die picture 1, 1 = die picture 2, etc.
+            dieValue_1 = rand_1.Next(5);
+            dieValue_2 = rand_2.Next(5);
+
+            // Display the value rolled for die_1.
+            switch (dieValue_1)
+            {
+                // Display dieValue_1
+                case 0:
+                    diePictureBox1_1.Visible = true;
+                    diePictureBox2_1.Visible = false;
+                    diePictureBox3_1.Visible = false;
+                    diePictureBox4_1.Visible = false;
+                    diePictureBox5_1.Visible = false;
+                    diePictureBox6_1.Visible = false;
+                    break;
+                case 1:
+                    diePictureBox1_1.Visible = false;
+                    diePictureBox2_1.Visible = true;
+                    diePictureBox3_1.Visible = false;
+                    diePictureBox4_1.Visible = false;
+                    diePictureBox5_1.Visible = false;
+                    diePictureBox6_1.Visible = false;
+                    break;
+                case 2:
+                    diePictureBox1_1.Visible = false;
+                    diePictureBox2_1.Visible = false;
+                    diePictureBox3_1.Visible = true;
+                    diePictureBox4_1.Visible = false;
+                    diePictureBox5_1.Visible = false;
+                    diePictureBox6_1.Visible = false;
+                    break;
+                case 3:
+                    diePictureBox1_1.Visible = false;
+                    diePictureBox2_1.Visible = false;
+                    diePictureBox3_1.Visible = false;
+                    diePictureBox4_1.Visible = true;
+                    diePictureBox5_1.Visible = false;
+                    diePictureBox6_1.Visible = false;
+                    break;
+                case 4:
+                    diePictureBox1_1.Visible = false;
+                    diePictureBox2_1.Visible = false;
+                    diePictureBox3_1.Visible = false;
+                    diePictureBox4_1.Visible = false;
+                    diePictureBox5_1.Visible = true;
+                    diePictureBox6_1.Visible = false;
+                    break;
+                case 5:
+                    diePictureBox1_1.Visible = false;
+                    diePictureBox2_1.Visible = false;
+                    diePictureBox3_1.Visible = false;
+                    diePictureBox4_1.Visible = false;
+                    diePictureBox5_1.Visible = false;
+                    diePictureBox6_1.Visible = true;
+                    break;
+            }
+
+            // Display the value rolled for die_2.
+            switch (dieValue_2)
+            {
+                // Display dieValue_1
+                case 0:
+                    diePictureBox1_2.Visible = true;
+                    diePictureBox2_2.Visible = false;
+                    diePictureBox3_2.Visible = false;
+                    diePictureBox4_2.Visible = false;
+                    diePictureBox5_2.Visible = false;
+                    diePictureBox6_2.Visible = false;
+                    break;
+                case 1:
+                    diePictureBox1_2.Visible = false;
+                    diePictureBox2_2.Visible = true;
+                    diePictureBox3_2.Visible = false;
+                    diePictureBox4_2.Visible = false;
+                    diePictureBox5_2.Visible = false;
+                    diePictureBox6_2.Visible = false;
+                    break;
+                case 2:
+                    diePictureBox1_2.Visible = false;
+                    diePictureBox2_2.Visible = false;
+                    diePictureBox3_2.Visible = true;
+                    diePictureBox4_2.Visible = false;
+                    diePictureBox5_2.Visible = false;
+                    diePictureBox6_2.Visible = false;
+                    break;
+                case 3:
+                    diePictureBox1_2.Visible = false;
+                    diePictureBox2_2.Visible = false;
+                    diePictureBox3_2.Visible = false;
+                    diePictureBox4_2.Visible = true;
+                    diePictureBox5_2.Visible = false;
+                    diePictureBox6_2.Visible = false;
+                    break;
+                case 4:
+                    diePictureBox1_2.Visible = false;
+                    diePictureBox2_2.Visible = false;
+                    diePictureBox3_2.Visible = false;
+                    diePictureBox4_2.Visible = false;
+                    diePictureBox5_2.Visible = true;
+                    diePictureBox6_2.Visible = false;
+                    break;
+                case 5:
+                    diePictureBox1_2.Visible = false;
+                    diePictureBox2_2.Visible = false;
+                    diePictureBox3_2.Visible = false;
+                    diePictureBox4_2.Visible = false;
+                    diePictureBox5_2.Visible = false;
+                    diePictureBox6_2.Visible = true;
+                    break;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
