d458b962e9cc5cbae4e06a54681f93dc08c85a6c
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:40:27 -0500

    Add files via upload

---
+++ 07_Total Sales/Total Sales/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Total_Sales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.getSalesButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.totalSalesLabel = new System.Windows.Forms.Label();
+            this.totalSalesDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.ItemHeight = 20;
+            this.outputListBox.Location = new System.Drawing.Point(41, 29);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(369, 184);
+            this.outputListBox.TabIndex = 0;
+            // 
+            // getSalesButton
+            // 
+            this.getSalesButton.Location = new System.Drawing.Point(108, 284);
+            this.getSalesButton.Name = "getSalesButton";
+            this.getSalesButton.Size = new System.Drawing.Size(113, 51);
+            this.getSalesButton.TabIndex = 1;
+            this.getSalesButton.Text = "Get Sales";
+            this.getSalesButton.UseVisualStyleBackColor = true;
+            this.getSalesButton.Click += new System.EventHandler(this.getSalesButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(240, 284);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(113, 51);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // totalSalesLabel
+            // 
+            this.totalSalesLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalSalesLabel.Location = new System.Drawing.Point(271, 222);
+            this.totalSalesLabel.Name = "totalSalesLabel";
+            this.totalSalesLabel.Size = new System.Drawing.Size(139, 39);
+            this.totalSalesLabel.TabIndex = 3;
+            this.totalSalesLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // totalSalesDescription
+            // 
+            this.totalSalesDescription.AutoSize = true;
+            this.totalSalesDescription.Location = new System.Drawing.Point(173, 232);
+            this.totalSalesDescription.Name = "totalSalesDescription";
+            this.totalSalesDescription.Size = new System.Drawing.Size(92, 20);
+            this.totalSalesDescription.TabIndex = 4;
+            this.totalSalesDescription.Text = "Total Sales:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 362);
+            this.Controls.Add(this.totalSalesDescription);
+            this.Controls.Add(this.totalSalesLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.getSalesButton);
+            this.Controls.Add(this.outputListBox);
+            this.Name = "Form1";
+            this.Text = "Total Sales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button getSalesButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label totalSalesLabel;
+        private System.Windows.Forms.Label totalSalesDescription;
+    }
+}
+
---
+++ 07_Total Sales/Total Sales/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Total_Sales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The Total method accepts an int array argument
+        // and returns the Total of the values in the array.
+        private double Total(double[] dArray)
+        {
+            // Declare and initialize an accumulator variable.
+            double total = 0;
+
+            // Step through the array, adding each element to
+            // the accumulator.
+            for (int index = 0; index < dArray.Length; index++)
+            {
+                total += dArray[index];
+            }
+
+            // Return the total.
+            return total;
+        }
+
+        private void getSalesButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Create an array to hold items read from the file.
+                const int SIZE = 7;             // Number of sales
+                double[] sales = new double[SIZE];    // Array of sales
+                double totalSales;              // To hold the total sales
+
+                // Counter variable to use int he loop
+                int index = 0;
+
+                // Declare a StreamReader variable.
+                StreamReader inputFile;
+
+                // Open the file and get a StreamReader object.
+                inputFile = File.OpenText("Sales.txt");
+
+                // Read the file's contents into the array.
+                while (index < sales.Length && !inputFile.EndOfStream)
+                {
+                    sales[index] = Double.Parse(inputFile.ReadLine());
+                    index++;
+                }
+
+                // Close the file.
+                inputFile.Close();
+
+                // Display the array elemetns in the list box.
+                foreach (double values in sales)
+                {
+                    outputListBox.Items.Add(values);
+                }
+
+                // Get the total sales.
+                totalSales = Total(sales);
+
+                // Display the value.
+                totalSalesLabel.Text = totalSales.ToString();
+            }
+            catch (Exception ex)
+            {
+                // Display an error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            this.Close();
+        }
+    }
+}
