174cd86e445c8f11fb72096003a0677d57fe13b5
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 18:37:22 -0500

    Add files via upload

---
+++ 07_Lottery Numbers/Lottery Numbers/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Lottery_Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstLabel = new System.Windows.Forms.Label();
+            this.secondLabel = new System.Windows.Forms.Label();
+            this.thirdLabel = new System.Windows.Forms.Label();
+            this.fourthLabel = new System.Windows.Forms.Label();
+            this.fifthLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstLabel
+            // 
+            this.firstLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.firstLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstLabel.Location = new System.Drawing.Point(21, 14);
+            this.firstLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.firstLabel.Name = "firstLabel";
+            this.firstLabel.Size = new System.Drawing.Size(63, 35);
+            this.firstLabel.TabIndex = 0;
+            this.firstLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.secondLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondLabel.Location = new System.Drawing.Point(93, 14);
+            this.secondLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(63, 35);
+            this.secondLabel.TabIndex = 1;
+            this.secondLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // thirdLabel
+            // 
+            this.thirdLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.thirdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdLabel.Location = new System.Drawing.Point(165, 14);
+            this.thirdLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.thirdLabel.Name = "thirdLabel";
+            this.thirdLabel.Size = new System.Drawing.Size(63, 35);
+            this.thirdLabel.TabIndex = 2;
+            this.thirdLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fourthLabel
+            // 
+            this.fourthLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fourthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fourthLabel.Location = new System.Drawing.Point(237, 14);
+            this.fourthLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fourthLabel.Name = "fourthLabel";
+            this.fourthLabel.Size = new System.Drawing.Size(63, 35);
+            this.fourthLabel.TabIndex = 3;
+            this.fourthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fifthLabel
+            // 
+            this.fifthLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fifthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fifthLabel.Location = new System.Drawing.Point(309, 14);
+            this.fifthLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fifthLabel.Name = "fifthLabel";
+            this.fifthLabel.Size = new System.Drawing.Size(63, 35);
+            this.fifthLabel.TabIndex = 4;
+            this.fifthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(80, 80);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(112, 52);
+            this.generateButton.TabIndex = 5;
+            this.generateButton.Text = "Generate Numbers";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(201, 80);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 52);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 152);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.fifthLabel);
+            this.Controls.Add(this.fourthLabel);
+            this.Controls.Add(this.thirdLabel);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.firstLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Lottery Numbers";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstLabel;
+        private System.Windows.Forms.Label secondLabel;
+        private System.Windows.Forms.Label thirdLabel;
+        private System.Windows.Forms.Label fourthLabel;
+        private System.Windows.Forms.Label fifthLabel;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
